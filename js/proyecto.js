91628c27bd250f491d514826d5edfc8b6b106882
juanfuertes <juancruzfuertes@gmail.com>
2022-10-06 17:31:02 -0300

    segunda entrega proyecto final

--- js/proyecto.js
+++ js/proyecto.js
@@ -155,8 +155,66 @@
 // });
 
 
+// suma = 0;
+// cantidadPedida = 0;
+
+// //Class Producto
+// class Producto {
+//   constructor(id, nombre, precio, stock, imagen, cantidadPedida) {
+//     this.id = Number(id);
+//     this.nombre = nombre.toUpperCase();
+//     this.precio = parseFloat(precio);
+//     this.stock = Number(stock);
+//     this.imagen = imagen;
+//     this.cantidadPedida = Number(cantidadPedida);
+//   }
+//   sumaIva() {
+//     this.precio = this.precio * 1.21;
+//   }
+//   restoStock(cantidad) {
+//     this.stock = this.stock - cantidad;
+//   }
+
+//   acumuloCantidad(cantidad) {
+//     this.cantidadPedida = this.cantidadPedida + cantidad;
+//   }
+// }
+
+// let producto1 = new Producto(1, "Cdj", 8000, 8,);
+// let producto2 = new Producto(2, "Mixer", 4000, 6,);
+// let producto3 = new Producto(3, "Boofer", 5000, 10,);
+// let producto4 = new Producto(4, "Soportes", 2000, 16,);
+
+// const productos = [producto1, producto2, producto3, producto4];
+
+// function mostrarProductos() {
+//   let html = "";
+//   for (let i = 0; i < productos.length; i++) {
+//     html =
+//       html +
+//       `<div class="tarjeta">
+//         <div><img class="imagen" src=${productos[i].imagen} /></div>
+//         <h2> ${productos[i].nombre}</h2>
+//         <p>Precio: ${productos[i].precio}</p>
+//         <div><small>Stock: ${productos[i].stock}</small>
+//         <small>Id: ${productos[i].id}</small> </div>
+//         <button id="btnAgregar">Agregar al Carro</button> 
+//         </div>`;
+
+//     document.getElementById("contenedor").innerHTML = html;
+//   }
+// }
+
+// mostrarProductos();
+// let boton = document.getElementById("btnAgregar(" + productos[i].id + ")");
+// boton.addEventListener("click", respuestaClick);
+// function respuestaClick(id) {
+//     console.log("Respuesta producto agregado: " + id);
+// }
+
 suma = 0;
 cantidadPedida = 0;
+
 
 //Class Producto
 class Producto {
@@ -174,40 +232,100 @@ class Producto {
   restoStock(cantidad) {
     this.stock = this.stock - cantidad;
   }
+  /*
+  calculoSubtotal() {
+    this.precio = this.precio * cantidad;
+  }*/
 
   acumuloCantidad(cantidad) {
     this.cantidadPedida = this.cantidadPedida + cantidad;
   }
 }
 
-let producto1 = new Producto(1, "Cdj", 8000, 8,);
-let producto2 = new Producto(2, "Mixer", 4000, 6,);
-let producto3 = new Producto(3, "Boofer", 5000, 10,);
-let producto4 = new Producto(4, "Soportes", 2000, 16,);
+let producto1 = new Producto(1, "Cdj", 8000, 8, 0);
+let producto2 = new Producto(2, "Mixer", 4000, 6, 0);
+let producto3 = new Producto(3, "Boofer", 5000, 10, 0);
+let producto4 = new Producto(4, "Parlantes", 4000, 15, 0);
+let producto5 = new Producto(5, "Soportes", 2000, 30, 0);
+let producto6 = new Producto(6, "Crm", 8, 3000, 0);
 
-const productos = [producto1, producto2, producto3, producto4];
+const productos = [producto1, producto2, producto3, producto4, producto5, producto6];
+const carro = [];
 
 function mostrarProductos() {
   let html = "";
   for (let i = 0; i < productos.length; i++) {
     html =
       html +
-      `<div class="tarjeta">
-        <div><img class="imagen" src=${productos[i].imagen} /></div>
-        <h2> ${productos[i].nombre}</h2>
-        <p>Precio: ${productos[i].precio}</p>
-        <div><small>Stock: ${productos[i].stock}</small>
-        <small>Id: ${productos[i].id}</small> </div>
-        <button id="btnAgregar">Agregar al Carro</button> 
-        </div>`;
+      `<div class="col">
+          <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3">
+              <h4 class="my-0 fw-normal">${productos[i].nombre}</h4>
+            </div>
+            <div class="card-body">
+              <h1 class="card-title pricing-card-title">$${productos[i].precio}</h1>
+                <img style="width:200px;height:200px;" src="${productos[i].imagen}" />
+                  <ul class="list-unstyled mt-3 mb-4">
+                    <li><small class="text-muted fw-light">stock: ${productos[i].stock}</small></li>
+                    <li><small class="text-muted fw-light">sku: ${productos[i].id}</small></li>
+                  </ul>
+                  <span class="text-muted fw-light" >Cantidad: <input style="margin: 6px;width: 50px;" class="text-muted fw-light" type="number" id="cantidad" value></span>
+                <button onclick="agregarAlCarro(${productos[i].id});" type="button" class="w-100 btn btn-lg btn-outline-primary">Comprar</button>
+            </div>
+          </div>
+      </div>`;
 
-    document.getElementById("contenedor").innerHTML = html;
+  }
+
+  document.getElementById("mis_cards").innerHTML = html;
+}
+
+
+
+function renderCarro() {
+  if (carro.length == 0) {
+    document.getElementById("carro").innerHTML =
+      "<h3>NO HAY NADA EN EL CARRO</h3>";
+  } else {
+    //let cantidad = document.getElementById("cantidad").value;
+    //alert("cantidad")
+    let html = "";
+    for (let i = 0; i < carro.length; i++) {
+      html =
+        html +
+        `
+      <div style="border: 1px solid green;margin: 10px;">
+      <p>id: ${carro[i].id}</p>
+      <p>nombre: ${carro[i].nombre}</p>
+      <p>precio: ${carro[i].precio}</p>
+     
+      <p>
+      <img style="width:200px;height:200px;" src="${carro[i].imagen}" />
+      </p>
+      <span style="cursor:pointer;" onclick="removeFromCart(${i});">🛒</span>
+      </div>
+      `;
+    }
+    document.getElementById("carro").innerHTML = html;
+
+    if (carro.length > 0) {
+      document.getElementById("hayCarro").innerHTML = "Mostrar carro";
+    }
   }
 }
 
 mostrarProductos();
-let boton = document.getElementById("btnAgregar(" + productos[i].id + ")");
-boton.addEventListener("click", respuestaClick);
-function respuestaClick(id) {
-    console.log("Respuesta producto agregado: " + id);
+renderCarro();
+
+
+
+function agregarAlCarro(id) {
+  const foundProduct = productos.find((item) => item.id == id);
+
+
+
+  //alert(JSON.stringify(foundProduct, null, 0));
+  carro.push(foundProduct);
+  //producto[id].restoStock();
+  renderCarro();
 }
